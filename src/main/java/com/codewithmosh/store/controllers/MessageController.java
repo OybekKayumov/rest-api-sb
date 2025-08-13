19e4804f4878fd569196091aabde221ff306562f
OybekKayumov <oybek.kayumov@gmail.com>
2025-08-13 07:56:56 +0500

    Add Message entity

---
+++ src/main/java/com/codewithmosh/store/controllers/MessageController.java
@@ -0,0 +1,21 @@
+package com.codewithmosh.store.controllers;
+
+import com.codewithmosh.store.entities.Message;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+  //* return String object
+  @RequestMapping("/hello")
+  public String sayHello() {
+    return "Hello World!";
+  }
+
+  //* return JSON object
+  @RequestMapping("/hello")
+  public Message sayHello2() {
+    return new Message("Hello World!");
+  }
+}

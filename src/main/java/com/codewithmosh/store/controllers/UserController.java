409513cae8379b6a5bd980c6006fcf5add685a3e
OybekKayumov <oybek.kayumov@gmail.com>
2025-08-13 08:40:42 +0500

    Add if user not found

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -3,6 +3,9 @@ package com.codewithmosh.store.controllers;
 import com.codewithmosh.store.entities.User;
 import com.codewithmosh.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,8 +38,17 @@ public class UserController {
 
   //@GetMapping("/users/{id}")
   @GetMapping("/{id}")
-  public User getUser(@PathVariable Long id) {
+  //public User getUser(@PathVariable Long id) {
+  public ResponseEntity<User> getUser(@PathVariable Long id) {
 
-    return userRepository.findById(id).orElse(null);
+    var user = userRepository.findById(id).orElse(null);
+
+    if (user == null) {
+      //return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+      ResponseEntity.notFound().build();
+    }
+
+    //return new ResponseEntity<>(user, HttpStatus.OK);
+    return ResponseEntity.ok(user);
   }
 }

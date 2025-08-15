fdc66d8b0dede729a70fbd04962bfcd468c70c61
OybekKayumov <oybek.kayumov@gmail.com>
2025-08-15 07:52:13 +0500

    Add Products

--- src/main/java/com/codewithmosh/store/controllers/ProductController.java
+++ src/main/java/com/codewithmosh/store/controllers/ProductController.java
@@ -1,8 +1,10 @@
 package com.codewithmosh.store.controllers;
 
 import com.codewithmosh.store.dtos.ProductDto;
+import com.codewithmosh.store.mappers.ProductMapper;
 import com.codewithmosh.store.repositories.ProductRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,13 +16,17 @@ import java.util.List;
 public class ProductController {
 
   private final ProductRepository productRepository;
+  private final ProductMapper productMapper;
 
 //  public ProductController(ProductRepository productRepository) {
 //    this.productRepository = productRepository;
 //  }
 
+  @GetMapping
   public List<ProductDto> getAllProducts() {
 
-    productRepository.findAll();
+    return productRepository.findAll().stream()
+            .map(productMapper::toDto)
+            .toList();
   }
 }
